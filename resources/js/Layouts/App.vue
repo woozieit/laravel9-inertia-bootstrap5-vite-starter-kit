935f1e5990f6d411a6ff2c8ab61aaf7c49ef80ca
Angel Rojas <angelitp.arp.0704@gmail.com>
2022-10-09 16:52:51 -0500

    Project Initial

---
+++ resources/js/Layouts/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+    import { Link } from '@inertiajs/inertia-vue3';
+</script>
+
+<template>
+    <div class="container py-4 px-3 mx-auto">
+        <slot />
+    </div>
+</template>
---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,22 @@
+<script setup>
+    import { Head, Link } from '@inertiajs/inertia-vue3';
+    import AppLayout from '@/Layouts/App.vue'
+
+    defineProps({
+        laravelVersion: String,
+        phpVersion: String
+    })
+</script>
+
+<template>
+    <AppLayout>
+        <Head title="Bienvenido" />
+
+        <h1>Hello, Laravel 9 Inertia JS - Vue 3 Project using Bootstrap and Vite!</h1>
+        <button class="btn btn-primary">Primary button</button>
+    </AppLayout>
+</template>
+
+<style scoped>
+
+</style>
